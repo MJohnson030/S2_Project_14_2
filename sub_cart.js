140281f0143cf1f086116b12761c2e0f60e1a0f2
Unknown <mjohnson030@west-mec.org>
2019-04-05 18:26:15 -0700

    Applied Nodes
    Created functions and inside the functions created nodes. Created a function that creates a shopping cart on the webpage and another function that allows you to add items into the cart. Started debugging.

---
+++ sub_cart.js
@@ -0,0 +1,59 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3 and JavaScript 6th Edition
+   Tutorial 12
+   Case Problem 2
+
+   Author: Millione Johnson
+   Date: 4/5/19  
+
+   Filename: sub_cart.js
+
+
+   Functions List:
+   setupCart() 
+      Sets up the event handlers for the Add to Order buttons on the web page.
+      
+   addItem(e)
+      Adds the food item associated with the Add to Order button to the shopping
+      cart, keeping track of the number of items of each product ordered by 
+      the customer.
+
+*/
+
+window.addEventListener("load", setupCart);
+
+function setupCart() {
+      var addButtons = document.getElementsByClassName("addButton");
+      // document.getElementsByClassName("addButton").onclick = addItem();
+
+      for (var i = 0; i < addButtons.length; i++) {
+            addButtons[i].onclick = addItem;
+      }
+}
+
+function addItem(e) {
+      var foodItem = e.target.nextElementSibling;
+      var foodID = fooditem.getAttribute("id");
+      var foodDescription = foodItem.cloneNode(true);
+      var cartBox = document.getElementById("cart");
+      var duplicateOrder = false;
+
+      for (var n = cartBox.firstChild; n = n.nextElementSibling; n !== null) {
+
+            if (n.id === foodID) {
+                  duplicateOrder = true;
+                  n.firstElementChild.textContent++;
+                  break;
+            }
+      }
+
+      if (duplicateOrder === false) {
+            var orderCount = document.createElement("span");
+            orderCount.textContent = "1";
+            foodDescription.insertBefore(orderCount, foodDescription.firstChild);
+            cartBox.appendChild(foodDescription);
+
+      }
+}
